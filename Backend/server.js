bad003cf69ed32d0708bfe0a138b6e638bd53fc4
irinesara <irine.ixa@gmail.com>
2025-04-01 12:03:12 +0530

    code done

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require("./src/Database/db");
+const userrouter = require('./src/Controllers/user');
 const app = express();
 
 require('dotenv').config({
@@ -18,6 +19,7 @@ app.listen(port, async() => {
     console.error(error);
   }
 });
+app.use('/auth',userrouter)
 
 app.get('/', (req, res) => {    
     res.send('Hello World!');
